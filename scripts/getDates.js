4c970bd59d8d7b4529458c4cfe0f557c6c86dc03
Your Name <your.email@example.com>
2025-03-11 16:48:06 +0000

    update added

---
+++ scripts/getDates.js
@@ -0,0 +1,6 @@
+// Update current year in footer
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Update last modified date
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
